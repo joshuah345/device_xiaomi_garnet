90138533f606bb5bc9490a36f948af4b1b8ab947
krasCGQ <krasCGQ@users.noreply.github.com>
2024-05-30 10:31:48 +0000

    garnet: Inherit several Android Go configurations Browse files Beneficial for everyone, even on non-Go devices.
    Signed-off-by: 01soni247 <01soni247@gmail.com>

--- device.mk
+++ device.mk
@@ -260,6 +260,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     IFAAService
 
+# Inherit several Android Go Configurations (Beneficial 
+# for everyone, even on non-Go devices)
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Init
 PRODUCT_PACKAGES += \
     charger_fw_fstab.qti \
