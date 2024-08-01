3755d0f9b6dd69b2a2e25651d5b40fa11fa629e6
Joshua Henry <joshuah345@gmail.com>
2024-08-01 13:24:10 -0400

    Rising extras (lineage_garnet.mk)

--- lineage_garnet.mk
+++ lineage_garnet.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Evolution X stuff.
+# Inherit some common Rising stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from garnet device
@@ -16,6 +16,12 @@ $(call inherit-product, device/xiaomi/garnet/device.mk)
 
 # Inherit from the MiuiCamera setup
 $(call inherit-product-if-exists, vendor/xiaomi/garnet-miuicamera/products/miuicamera.mk)
+
+ifeq ($(strip $(WITH_GMS)),true)
+    $(call inherit-product, vendor/bcr/bcr.mk)
+    TARGET_DEFAULT_PIXEL_LAUNCHER := true
+endif
+
 
 TARGET_BUILD_APERTURE_CAMERA := false
 
