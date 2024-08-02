d52ff038980c49add8911e6617f7fdd66b898b61
Joshua Henry <joshuah345@gmail.com>
2024-08-02 05:34:50 -0400

    Rename garnet-kernel to garnet-prebuilt
    Consistency and compatibility is important here.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,7 +129,7 @@ BOARD_BOOTCONFIG := \
     androidboot.usbcontroller=a600000.dwc3
 
 # Kernel (prebuilt)
-PREBUILT_PATH := device/xiaomi/garnet-kernel
+PREBUILT_PATH := $(DEVICE_PATH)-prebuilt
 BOARD_PREBUILT_DTBIMAGE_DIR := $(PREBUILT_PATH)/images/dtbs/
 BOARD_PREBUILT_DTBOIMAGE := $(PREBUILT_PATH)/images/dtbo.img
 
