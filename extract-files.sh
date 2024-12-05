cd48a3dc76e9fee13290dc79fb46caffe13a62c4
kleidione Freitas <kleidione@gmail.com>
2024-12-05 13:01:37 -0500

    garnet: Add permission for gettid syscall in sensors-qesdk
    Problem:
    During startup of the `vendor.sensors.qesdk` service, seccomp blocks the `gettid` syscall, causing repeated failures and forcing the service to terminate. Excerpt from the log illustrating the error:
    
    11-26 20:02:58.656 10336 10336 E sensors-qesdk: libminijail[10336]: blocked syscall: gettid
    
    Fix:
    Added permission for the `gettid` syscall in the `seccomp_policy/sensors-qesdk.policy` policy file via the `extract-files.sh` script, ensuring that the service can run without interruption.

--- extract-files.sh
+++ extract-files.sh
@@ -70,9 +70,12 @@ function blob_fixup() {
         vendor/etc/media_codecs_parrot_v0.xml)
             sed -i -E '/media_codecs_(google_audio|google_c2|google_telephony|vendor_audio)/d' "${2}"
             ;;
-        vendor/etc/seccomp_policy/atfwd@2.0.policy)
-            [ "$2" = "" ] && return 0
-            grep -q "gettid: 1" "${2}" || echo "gettid: 1" >> "${2}"
+        vendor/etc/seccomp_policy/atfwd@2.0.policy|vendor/etc/seccomp_policy/wfdhdcphalservice.policy|vendor/etc/seccomp_policy/sensors-qesdk.policy)
+            [ -z "$2" ] || [ ! -f "$2" ] && return 0
+            [ -n "$(tail -c 1 "${2}")" ] && echo >> "${2}"
+            sed -i '/gettid: 1/d' "${2}"
+            sed -i '/^[[:space:]]*$/d' "${2}"
+            echo "gettid: 1" >> "${2}"
             ;;
         vendor/etc/seccomp_policy/c2audio.vendor.ext-arm64.policy)
             [ "$2" = "" ] && return 0
